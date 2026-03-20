30d7f4707d54c7db7b45337c1c3750839745851e
Thobias Tinnerholm <Thobias.tinnerholm@gmail.com>
2026-03-20 15:30:25 +0100

    added perm

---
+++ permutations/permutations.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+
+void solve(char *perm, int *used, char *str, int pos, int n)
+{
+    int i = 0;
+
+    if (pos == n)
+    {
+        printf("%s\n", perm);
+        return;
+    }
+
+    while (i < n)
+    {
+        if (!used[i])
+        {
+            used[i] = 1;
+            perm[pos] = str[i];
+            perm[pos + 1] = '\0';
+
+            solve(perm, used, str, pos + 1, n);
+
+            used[i] = 0;
+        }
+        i++;
+    }
+}
+
+void sort(char *s)
+{
+    int i = 0;
+    int j;
+    char tmp;
+
+    while (s[i])
+    {
+        j = i + 1;
+        while (s[j])
+        {
+            if (s[i] > s[j])
+            {
+                tmp = s[i];
+                s[i] = s[j];
+                s[j] = tmp;
+            }
+            j++;
+        }
+        i++;
+    }
+}
+
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+        return 1;
+
+    char *str = argv[1];
+
+    sort(str);
+
+    int n = 0;
+    while (str[n])
+        n++;
+
+    char perm[n + 1];
+    int used[n];
+
+    for (int i = 0; i < n; i++)
+        used[i] = 0;
+
+    solve(perm, used, str, 0, n);
+
+    return 0;
+}
